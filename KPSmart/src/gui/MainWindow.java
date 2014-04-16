7bbbfff6d5a6df2967d9c8748e5b1f91d55dc772
genkenel <alexc1@windowslive.com>
2014-04-16 20:34:57 +1200

    Starting on the next part of the GUI

--- KPSmart/src/gui/MainWindow.java
+++ KPSmart/src/gui/MainWindow.java
@@ -2,8 +2,7 @@ package gui;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class MainWindow extends JFrame{
 
--- KPSmart/src/gui/Transitioner.java
+++ KPSmart/src/gui/Transitioner.java
@@ -1,5 +1,14 @@
 package gui;
 
-public class Transitioner {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Transitioner implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
