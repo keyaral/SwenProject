dc7033b6559ee0bf2dbe5da88871ec86595fcc58
genkenel <alexc1@windowslive.com>
2014-04-20 15:43:21 +1200

    Progress on GUI

--- KPSmart/src/gui/Transitioner.java
+++ KPSmart/src/gui/Transitioner.java
@@ -4,7 +4,12 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Transitioner implements ActionListener{
-
+	
+	private boolean requiresConfirmation;
+	private JComponent destination;
+	
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
