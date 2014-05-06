3a614caa27ddf12a8d78504edfe4d0750e2a0fae
Marian Clements <maz-firebabe@hotmail.com>
2014-05-06 21:05:08 +1200

    Added Destination class to help with Stats, mail and KP Costs

---
+++ KPSmart/src/Logic/Destination.java
@@ -0,0 +1,78 @@
+package Logic;
+
+public class Destination {
+
+private String name;	
+	
+	
+
+
+private boolean domestic; 
+
+private double domesticVolume = 0.0;
+private double internationVolume = 0.0;
+private double domesticWeightS = 0.0;
+private double internationWeight = 0.0;
+
+
+public Destination(String name, double domesticVolume,
+		double internationVolume, double domesticWeightS,
+		double internationWeight) {
+	super();
+	this.name = name;
+	this.domesticVolume = domesticVolume;
+	this.internationVolume = internationVolume;
+	this.domesticWeightS = domesticWeightS;
+	this.internationWeight = internationWeight;
+} 
+	
+public Destination(String name, Boolean dom) {
+	this.name= name;
+	this.domestic = dom;
+}
+
+
+
+public  void addDomesticVolume(double dv) {
+	 domesticVolume += dv;
+}
+
+public  void addInternationVolume(double iv) {
+	 internationVolume += iv;
+}
+
+public  void addDomesticWeightS( double dw ) {
+	 domesticWeightS += dw;
+}
+
+public void addInternationWeight(double iw) {
+	 internationWeight += iw;
+}
+
+
+public  String getName() {
+	return name;
+}
+
+public  double getDomesticVolume() {
+	return domesticVolume;
+}
+
+public  double getInternationVolume() {
+	return internationVolume;
+}
+
+public  double getDomesticWeightS() {
+	return domesticWeightS;
+}
+
+public  double getInternationWeight() {
+	return internationWeight;
+}
+
+public  boolean isDomestic() {
+	return domestic;
+}
+
+}
+
