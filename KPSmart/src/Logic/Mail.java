4ad2963028820c8bc99ad43fd59090cb3db930a5
Marian Clements <maz-firebabe@hotmail.com>
2014-05-06 21:56:56 +1200

    More destinations in MailDelivery

--- KPSmart/src/Logic/Mail.java
+++ KPSmart/src/Logic/Mail.java
@@ -5,18 +5,18 @@ import java.util.Date;
 public class Mail {
 
 	public int ID;
-	public Destination destination;
+	public String destination;
 	public String origin;
 	public double weight;
 	public double volume;
 	public int priority;
 	public Date date;
 	public double cost;
-	
+	public String name;
 	
 	public Mail(int i, String de, String o, double w, double v, int p, Date da){
 		this.ID = i;
-		this.destination = new Destination(o, inNewZealand(o) );
+		this.name = o;
 		this.origin = o;
 		this.weight = w;
 		this.volume = v;
@@ -24,18 +24,12 @@ public class Mail {
 		this.date = da;
 	}
 
-	private Boolean inNewZealand(String o) {
-		// TODO Auto-generated method stub
-		//if o matches list
-		
-		
-		return true;
-	}
+
 
 	public Mail(String details) {
 		String[] values = details.split("\t");
 		this.ID = Integer.parseInt(values[0]);
-		this.destination = new Destination(values[1], inNewZealand(values[1]) );
+		this.destination = (values[1]);
 		this.origin = (values[2]);
 		this.weight = Integer.parseInt(values[3]);
 		this.volume = Integer.parseInt(values[4]);
--- KPSmart/src/Logic/MailDelivery.java
+++ KPSmart/src/Logic/MailDelivery.java
@@ -1,9 +1,13 @@
 package Logic;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class MailDelivery {
 
+    public ArrayList<Destination> nzDestinations = new ArrayList<Destination>();	
+    public ArrayList<Destination> overSeasDestinations = new ArrayList<Destination>();
+    
 	public int totalVolume = 0;
 	public int totalWeight = 0;
 	public int totalitems = 0;
@@ -19,9 +23,13 @@ public class MailDelivery {
 	
 
 	public MailDelivery() {
-		
-		
-		
+		nzDestinations.add(new Destination("Wellington", true) );
+		nzDestinations.add(new Destination("Auckland", true) );
+		nzDestinations.add(new Destination("Dunedin", true) );
+		nzDestinations.add(new Destination("Rotarua", true) );
+		nzDestinations.add(new Destination("Palmerston North", true) );
+		nzDestinations.add(new Destination("Christchuch", true) );
+		nzDestinations.add(new Destination("Wellington", true) );
 		// TODO Auto-generated constructor stub
 	}
 
@@ -38,19 +46,63 @@ public class MailDelivery {
 	double routeVcost =	m.volume *r.costVolume;
 	double routeWcost = m.volume *r.costWeight;
 	
+	
 	double kpCost = Math.max(kpVcost, kpWcost);
 	double routeCost = Math.max(routeVcost, routeWcost);
 	
 	 tRevenue += kpCost;
 	 tExpediture += routeCost;
-	 
+
+	 Destination origin = findOrigin(m.origin);
+	 Destination dest = findDestination(m.destination);
 	 // handle Delivery time
 	 // handle amount of mail
-
+	 if (dest.isDomestic()) {
+		 origin.addDomesticVolume(m.volume);
+		 origin.addDomesticWeightS(m.weight);
+	 }
 	
-			return true;
-	
+	 else {
+		 origin.addInternationVolume(m.volume);
+		 origin.addInternationWeight(m.weight);
+		 
+	 }
+	return true;
 	}
+	
+	private Destination findOrigin(String origin) {
+		Boolean dom = false;
+		for (Destination d :nzDestinations ) {
+			if ( origin.equals(d.getName()) ) {
+				return d;  }
+		}
+	if (!dom) {//TODO ERROR}	
+		}
+	return null;
+	}
+	
+	private Destination findDestination(String des) {
+		Boolean dom = false;
+		for (Destination d :nzDestinations ) {
+			if ( des.equals(d.getName()) ) {
+				return d;  }
+		}
+	
+		
+			for (Destination os :overSeasDestinations ) {
+				if ( des.equals(os.getName()) ) {
+					return os;  }
+			}
+		
+		Destination newOs = new Destination(des, false);	
+		overSeasDestinations.add(newOs);	
+			return newOs;
+		}
+	
+	
+	
+	
+	
 	
 	public int getTotalVolume() {
 		return totalVolume;
