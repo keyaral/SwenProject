16a8472b9c3ec44bcb2995dde9377d3685c57406
Marian Clements <maz-firebabe@hotmail.com>
2014-05-11 08:52:10 +1200

    More mail logic

--- KPSmart/src/Logic/Logic.java
+++ KPSmart/src/Logic/Logic.java
@@ -39,16 +39,16 @@ public class Logic {
 		processform( new String[] {"0","4125","Auckland", "Wellington","8","10","200","200","2","Tuesday", "5", "3","KpStart"} );	
 		
 		
-		processform( new String[] {"0","4127","Rotarua", "Wellington","8","10","200","200","1","Tuesday", "5", "3","KpStart"} );	
-		processform( new String[] {"0","4128","Wellington", "Rotarua","8","10","200","200","1","Tuesday", "5", "3","KpStart"} );	
-		processform( new String[] {"0","4129","Auckland", "Rotarua","8","10","200","200","1","Tuesday", "5", "3","KpStart"} );	
-		processform( new String[] {"0","4120","Rotarua", "Auckland","8","10","200","200","1","Tuesday", "5", "3","KpStart"} );	
-		processform( new String[] {"0","4125","Wellington", "Auckland","8","10","200","200","1","Tuesday", "5", "3","KpStart"} );	
+		System.out.println(	processform( new String[] {"0","4127","Rotarua", "Wellington","8","10","200","200","1","Tuesday", "5", "3","KpStart"} ));	
+		System.out.println(	processform( new String[] {"0","4128","Wellington", "Rotarua","8","10","200","200","1","Tuesday", "5", "3","KpStart"} ));	
+		System.out.println(	processform( new String[] {"0","4129","Auckland", "Rotarua","8","10","200","200","1","Tuesday", "5", "3","KpStart"} ));	
+		System.out.println(	processform( new String[] {"0","4120","Rotarua", "Auckland","8","10","200","200","1","Tuesday", "5", "3","KpStart"} ));	
+		System.out.println(	processform( new String[] {"0","4125","Wellington", "Auckland","8","10","200","200","1","Tuesday", "5", "3","KpStart"} ));	
 		System.out.println("routes done ");
 		
 		
-		processform( new String[] {"5","5126","Rotarua", "Auckland","8","10","1", "10-17-2014"} );	
-		processform( new String[] {"5","5126","Rotarua", "Auckland","8","10","2", "10-17-2014"} );
+		System.out.println(	processform( new String[] {"5","5126","Rotarua", "Auckland","8","10","1", "10-17-2014"} ) );	
+		System.out.println(	processform( new String[] {"5","5126","Rotarua", "Auckland","8","10","2", "10-17-2014"} ) ) ;
 	System.out.println("mail done ");
 
 //		
