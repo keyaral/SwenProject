98b6e078a9a6aff989a43836953bf42bfd6b9799
Marian Clements <maz-firebabe@hotmail.com>
2014-05-20 20:55:41 +1200

    Fixed updating all changes

--- KPSmart/src/gui/MailDelivery.java
+++ KPSmart/src/gui/MailDelivery.java
@@ -142,10 +142,6 @@ public class MailDelivery extends JInternalFrame {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				clearFields();
-<<<<<<< HEAD
-				
-=======
->>>>>>> refs/remotes/origin/master
 			}
 			
 		});
@@ -171,26 +167,18 @@ public class MailDelivery extends JInternalFrame {
 					int dialogResult = JOptionPane.showConfirmDialog (null, "Are you sure you want to send this mail item?","Confirmation",dialogButton);
 					if(dialogResult == JOptionPane.YES_OPTION){
 						setFields(false);
-<<<<<<< HEAD
-=======
 
->>>>>>> refs/remotes/origin/master
 							
 							String[]  details ={ "5",txtMailId.getText(), destination ,
 											origin, txtWeight.getText(),
 											txtVolume.getText(),priority,txtDate.getText()  };
 							
 							JOptionPane.showMessageDialog(null,MainWindow.logic.processform(details),null, 1);
-							MainWindow.bMonitoring.updateMonitor();
-<<<<<<< HEAD
+
 		
 							setFields(true);
 							clearFields();
-=======
-							setFields(true);
-							clearFields();
 
->>>>>>> refs/remotes/origin/master
 				//		String type= MainWindow.logic.switchEvents(3,"3a"); //Case 3: Type "3a" Send  Mail
 						
 					
@@ -437,7 +425,7 @@ public class MailDelivery extends JInternalFrame {
 		cmbPriority.setSelectedIndex(-1);
 
 	}
-<<<<<<< HEAD
+
 		
 	 	private void clearFields() {
 	 		txtMailId.setText("");
@@ -465,35 +453,5 @@ public class MailDelivery extends JInternalFrame {
 	 		cmbPriority.setEnabled(b);
 	 		txtMailId.setEnabled(b);
 	 	}
-	 
-=======
-	
-	private void clearFields() {
-		txtMailId.setText("");
-		txtWeight.setText("");
-		txtVolume.setText("");
-		txtDate.setText("");
-		cmbDay.setSelectedIndex(-1);
-		cmbDestination.setSelectedIndex(-1);
-		cmbOrigin.setSelectedIndex(-1);
-		cmbPriority.setSelectedIndex(-1);
-		btnSave.setEnabled(true);
-		btnClearFields.setEnabled(false);
-	}
-	
-	private void setFields(boolean b) {
-		btnClose.setEnabled(b);
-		btnClearFields.setEnabled(b);
-		btnSave.setEnabled(b);
-		txtVolume.setEnabled(b);
-		txtWeight.setEnabled(b);
-		txtDate.setEnabled(b);
-		cmbOrigin.setEnabled(b);
-		cmbDestination.setEnabled(b);
-		cmbDay.setEnabled(b);
-		cmbPriority.setEnabled(b);
-		txtMailId.setEnabled(b);
-	}
-	
->>>>>>> refs/remotes/origin/master
+
 }
