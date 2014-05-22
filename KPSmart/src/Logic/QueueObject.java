cb3c9084bd2cb49f5dbc5fbcb454abee07f41c49
Marian Clements <maz-firebabe@hotmail.com>
2014-05-22 23:19:40 +1200

    Revert "Queue Object fix"
    This reverts commit 95e7dea20437c7d283e6a2fe5eddb90db72cbd57.

--- KPSmart/src/Logic/QueueObject.java
+++ KPSmart/src/Logic/QueueObject.java
@@ -1,7 +1,5 @@
 package Logic;
 
-import java.util.Comparator;
-
 public class QueueObject {
 
 	public QueueObject from;
@@ -15,12 +13,4 @@ public class QueueObject {
 		weight = w;
 		maxEstimate += estimate;
 	}
-
-	 public static Comparator<QueueObject> desComparator = new Comparator<QueueObject>(){
-		 	        @Override
-		 	        public int compare(QueueObject a, QueueObject b) {
-		 	            if(a.destination.GeographicalY < b.destination.GeographicalY) return 1;
-		 	            else return 0;
-		 	        }
-		 	    };
 }
